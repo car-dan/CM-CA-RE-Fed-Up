b87687adfd6891f2c11ec15bc87a8ef23e6243d5
car-dan <73527175+car-dan@users.noreply.github.com>
2021-12-31 09:55:08 +0100

    update 1

--- js/script.js
+++ js/script.js
@@ -1,19 +1,43 @@
 const url = "https://www.danielrud.no/wp-json/wp/v2/posts";
 const posts = document.querySelector(".posts");
 
-async function getPosts() {
-	const response = await fetch(url);
-	const json = await response.json();
+(async function getPosts() {
+	try {
+		const response = await fetch(url);
+		const json = await response.json();
 
-	console.log(json);
-	posts.innerHTML = "";
-
-	json.forEach((post) => {
-		posts.innerHTML += `<div class = "post">
+		console.log(json);
+		posts.innerHTML = "";
+		let numberOfPosts = json.length;
+		console.log(numberOfPosts);
+		json.forEach((post) => {
+			posts.innerHTML += `<div class = "post">
                                 <h2>${post.title.rendered}</h2>
                                 ${post.content.rendered}
                             </div>`;
-	});
+		});
+	} catch (error) {
+		console.log(error);
+	}
+})();
+
+function liveUpdate() {
+	setInterval(async function () {
+		numberOfPosts = document.querySelectorAll(".post").length;
+
+		try {
+			const response = await fetch(url);
+			const json = await response.json();
+
+			if (json.length != numberOfPosts) {
+				location.reload();
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}, 10000);
 }
 
-getPosts();
+document.addEventListener("DOMContentLoaded", function () {
+	liveUpdate();
+});
